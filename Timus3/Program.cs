222ce73701a7d775f2b0b4ce21edda8648803f71
GovjadkoIlja <govjadkoilja@yandex.ru>
2016-03-09 19:36:20 +0300

    .

---
+++ Timus3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timus2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum;
+            n=Convert.ToInt32(Console.ReadLine());
+            if (n < 0)
+                sum = -n * (n - 1) / 2+1;
+            else
+                sum= n * (n + 1) / 2;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
